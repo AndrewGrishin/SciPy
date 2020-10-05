b6e1d6c90ecc6b570c03fceb2f30bbd2e36c83ee
FelixMonteg <FelixMonteg@users.github.com>
2020-10-05 18:25:30 +0300

    Solving system and writing results system are done

--- linear_system/linear_system.py
+++ linear_system/linear_system.py
@@ -3,32 +3,45 @@ import numpy as np
 from random import randint
 import sys
 
-usedColumns = []
+def solveMatrixUsingGaussJordanMethod(matrix, b, rnd = None):
+    matrixLocal = np.copy(matrix)
+
+    matrixEquationTask = open("matrixEquationTask.txt","w")
+    matrixEquationTask.write("Initial matrix. Shape -> (" + str(len(matrixLocal)) + ", " + str(len(matrixLocal[0])) + ")\n\t")
+    for vector,index in zip(matrixLocal,range(len(matrixLocal))):
+        matrixEquationTask.write(" ".join(map(lambda f : str(format(f,"." + str(rnd) + "f") if rnd else f),vector)) + " = " + str(format(b[index],"." + str(rnd) + "f") if rnd else b[index]) + "\n\t")
+    matrixEquationTask.write("\n")
+
+    usedColumns = []
+    for vector,indVec in zip(matrixLocal,range(len(matrixLocal))):
+        for value, indVal in zip(vector,range(len(vector))):
+            if (value != 0 and indVal not in usedColumns):
+                usedColumns.append(indVal)
+                vector /= value
+                b[indVec] /= value
+                break
+        for ind in range(len(matrixLocal)):
+            if (sum(1 for val in vector if val == 0) == len(vector) and b[indVec]):
+                matrixEquationTask.write("This system is inconsistent!\n")
+                sys.exit(1)
+            if (ind == indVec): continue
+            if (matrixLocal[ind][usedColumns[-1]] != 0):
+                b[ind] -= b[indVec] * matrixLocal[ind][usedColumns[-1]]
+                matrixLocal[ind] -= matrixLocal[indVec] * matrixLocal[ind][usedColumns[-1]]
+
+    matrixEquationTask.write("Final matrix.\n\t")
+    for vector,index in zip(matrixLocal,range(len(matrixLocal))):
+        matrixEquationTask.write(" ".join(map(lambda f : str(format(f,"." + str(rnd) + "f") if rnd else f),vector)) + " = " + str(format(b[index],"." + str(rnd) + "f") if rnd else b[index]) + "\n\t")
+    matrixEquationTask.close()
+
+    del(usedColumns)
+    del(matrixLocal)
+
 
 matrix = np.array(
-    [[2.0,1.0,2.0],
-    [1.0,2.0,1.0],
-    [3.0,1.0,-1.0]]
+    [[2.0,1.0,7.0,5.0],
+    [1.0,2.0,1.0,3.0],]
 )
+b = np.array([1.0,-1.0])
 
-b = np.array([10.0,8.0,2.0])
-
-for vector,indVec in zip(matrix,range(len(matrix))):
-    for value, indVal in zip(vector,range(len(vector))):
-        if (value != 0 and indVal not in usedColumns):
-            usedColumns.append(indVal)
-            vector /= value
-            b[indVec] /= value
-            break
-    for ind in range(len(matrix)):
-        if (ind == indVec): continue
-        if (sum(1 for val in vector if val == 0) == len(vector) and b[indVec]):
-            print("This system is inconsistent!")
-            sys.exit(1)
-        if (matrix[ind][usedColumns[-1]] != 0):
-            b[ind] -= b[indVec] * matrix[ind][usedColumns[-1]] #vec[usedColumns[-1]]
-            matrix[ind] -= matrix[indVec] * matrix[ind][usedColumns[-1]] #vec[usedColumns[-1]]
-
-
-print(matrix)
-print(b)
+solveMatrixUsingGaussJordanMethod(matrix,b,3)

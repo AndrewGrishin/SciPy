d8f9fdc015b70d7750f95a4c7d51511f76c27ed2
FelixMonteg <FelixMonteg@users.github.com>
2020-10-05 12:43:53 +0300

    Gauss solution for linear system

--- linear_system/linear_system.py
+++ linear_system/linear_system.py
@@ -2,7 +2,43 @@ from scipy import linalg
 import numpy as np
 from random import randint
 
-n = randint(1,7)
-#A = np.array([[randint(-8,9) for i in range(n)] for j in range(n)])
-#b = np.array([randint(-50,51) for i in range(n)])
+used = []
+strings = 3
+columns = 3
 
+matrix = np.array(
+    [[5.0,0.0,3.0],
+    [1.0,4.0,7.0],
+    [0.0,0.0,2.0]]
+)
+
+b = np.array([3.0,1.0,8.0])
+
+for vector,indVec in zip(matrix,range(len(matrix))):
+    print(matrix)
+    print(b)
+    print()
+    for value, indVal in zip(vector,range(len(vector))):
+        if (value != 0 and indVal not in used):
+            used.append(indVal)
+            matrix[indVec] /= value
+            b[indVec] /= value
+            break
+    
+print(matrix)
+print(b)
+
+"""
+a b c | d1 (/a) (a != 0)
+e f g | d2
+h i j | d3
+
+1 b/a c/a | d1/a (*e)
+e   f   g | d2
+h   i   j | d3
+
+e e*b/a e*c/a | e*d1/a 
+e     f     g | d2
+h     i     j | d3
+
+"""

b01f34582d22b80a25cfe40be9d3032848f0ab5e
FelixMonteg <FelixMonteg@users.github.com>
2020-10-05 18:58:17 +0300

    Programme works. Bugs were not detected yet.

--- linear_system/linear_system.py
+++ linear_system/linear_system.py
@@ -37,11 +37,32 @@ def solveMatrixUsingGaussJordanMethod(matrix, b, rnd = None):
     del(usedColumns)
     del(matrixLocal)
 
+# start "try" -> "except" block, to check if the inputed file extsts
+try:
+    # try to open the file, the name of which was entered
+    file = open(input("Input the file name, where the matrix is: "),"r")
+except FileNotFoundError:
+    # Create a new file "errors.txt", to write all errors in
+    error = open("FileNotFoundError.txt","w")
+    # if error "FileNotFound" occures => there is no such file in this directory
+    # write into the file "errors.txt" that there is no such file in the current directory
+    error.write("FileNotFound: There is no such file in current directory! Please, try again.\n\n")
+    # write into the standart output (terminal), that a new file has been created
+    print("Check the file \"FileNotFoundError.txt\" a new error has just occured.")
+    # close "FileNotFoundError.txt" file
+    error.close()
+    # give the control back from the current programme to the Operation System
+    sys.exit(1)
 
-matrix = np.array(
-    [[2.0,1.0,7.0,5.0],
-    [1.0,2.0,1.0,3.0],]
-)
-b = np.array([1.0,-1.0])
+# if everything is "ok" => split the matrix from the file into the normal matrix
+# "matrix" is only n-1 rows and the last one is "b" vector
+# "*matrix" and "b" -> are <class, list> => need to convert them to NumPy arrays
+*matrix,b = np.array(np.array([list(map(float,line.split(" "))) for line in file]))
+# close the file with the input information
+file.close()
 
-solveMatrixUsingGaussJordanMethod(matrix,b,3)
+# call the function "solveMatrixUsingGaussJordanMethod" with numbers, rounded to the 3'rd digit
+solveMatrixUsingGaussJordanMethod(np.array(matrix),np.array(b),rnd = 3)
+# print into the standart output stream (terminal), that the programme has succeeded
+print("Process\t" + "-" * 13 + "> OK")
+print("Result\t" + "-" * 13 + "> \"matrixEquationTask.txt\"")

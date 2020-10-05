3c823bcd1efa55a733385207626421d49fb23219
FelixMonteg <FelixMonteg@users.github.com>
2020-10-05 11:10:54 +0300

    Change

--- linear_system/linear_system.py
+++ linear_system/linear_system.py
@@ -6,8 +6,3 @@ n = randint(1,7)
 #A = np.array([[randint(-8,9) for i in range(n)] for j in range(n)])
 #b = np.array([randint(-50,51) for i in range(n)])
 
-A = np.array([[1,2],[2,4]])
-b = np.array([3,6])
-x = linalg.solve(A,b)
-
-print(x)

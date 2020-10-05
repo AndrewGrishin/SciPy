15d44f74052ec91a9f74aca1089213be990410ac
FelixMonteg <FelixMonteg@users.github.com>
2020-10-05 16:56:17 +0300

    Almost Done

--- linear_system/linear_system.py
+++ linear_system/linear_system.py
@@ -1,44 +1,34 @@
 from scipy import linalg
 import numpy as np
 from random import randint
+import sys
 
-used = []
-strings = 3
-columns = 3
+usedColumns = []
 
 matrix = np.array(
-    [[5.0,0.0,3.0],
-    [1.0,4.0,7.0],
-    [0.0,0.0,2.0]]
+    [[2.0,1.0,2.0],
+    [1.0,2.0,1.0],
+    [3.0,1.0,-1.0]]
 )
 
-b = np.array([3.0,1.0,8.0])
+b = np.array([10.0,8.0,2.0])
 
 for vector,indVec in zip(matrix,range(len(matrix))):
-    print(matrix)
-    print(b)
-    print()
     for value, indVal in zip(vector,range(len(vector))):
-        if (value != 0 and indVal not in used):
-            used.append(indVal)
-            matrix[indVec] /= value
+        if (value != 0 and indVal not in usedColumns):
+            usedColumns.append(indVal)
+            vector /= value
             b[indVec] /= value
             break
-    
+    for ind in range(len(matrix)):
+        if (ind == indVec): continue
+        if (sum(1 for val in vector if val == 0) == len(vector) and b[indVec]):
+            print("This system is inconsistent!")
+            sys.exit(1)
+        if (matrix[ind][usedColumns[-1]] != 0):
+            b[ind] -= b[indVec] * matrix[ind][usedColumns[-1]] #vec[usedColumns[-1]]
+            matrix[ind] -= matrix[indVec] * matrix[ind][usedColumns[-1]] #vec[usedColumns[-1]]
+
+
 print(matrix)
 print(b)
-
-"""
-a b c | d1 (/a) (a != 0)
-e f g | d2
-h i j | d3
-
-1 b/a c/a | d1/a (*e)
-e   f   g | d2
-h   i   j | d3
-
-e e*b/a e*c/a | e*d1/a 
-e     f     g | d2
-h     i     j | d3
-
-"""

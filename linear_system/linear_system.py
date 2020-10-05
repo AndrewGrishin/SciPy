c061fa9ee2d9aef8bc2d0fa38947084498d25563
FelixMonteg <FelixMonteg@users.github.com>
2020-10-05 06:16:40 +0300

    Solver for square matrixs only

--- linear_system/linear_system.py
+++ linear_system/linear_system.py
@@ -3,5 +3,11 @@ import numpy as np
 from random import randint
 
 n = randint(1,7)
-a = np.array([[randint(-8,9) for i in range(n)] for j in range(n)])
-b = np.array([randint(-50,51) for i in range(n)])
+#A = np.array([[randint(-8,9) for i in range(n)] for j in range(n)])
+#b = np.array([randint(-50,51) for i in range(n)])
+
+A = np.array([[1,2],[2,4]])
+b = np.array([3,6])
+x = linalg.solve(A,b)
+
+print(x)

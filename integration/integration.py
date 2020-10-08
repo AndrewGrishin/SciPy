b13731ca0c703ec72e6e0539d11ab90a86ecc707
FelixMonteg <FelixMonteg@users.github.com>
2020-10-08 03:53:00 +0300

    The options of calculation

--- integration/integration.py
+++ integration/integration.py
@@ -5,18 +5,26 @@ import math
 import time
 
 def func(x):
-    return math.sin(x)
+    return math.sin(x) * math.cos(math.sin(x))
 
-def monteCarloIntegration(func, a = 0, b = 1, n = 10):
+def monteCarloIntegration(func, a = 0, b = 1, n = int(1E7)):
+    start = time.time()
     xvalues = a + (b - a) * np.random.uniform(low = 0, high = 1, size = n)
-    xvalues += (a + (b - a) * np.random.uniform(low = 0, high = 1, size = n))
-    plt.plot(xvalues)
-    fvalues = np.array(list(map(func,xvalues)))
-    plt.show()
-    print(sum(fvalues * (b - a) / n))
+    #fvalues = map(func,xvalues)
+    return (sum(map(func,xvalues)) * (b - a) / n, time.time() - start)
 
-    
+def mci(func, a = 1, b = 1, n = int(1E7)):
+    start1 = time.time()
+    subsets = np.arange(0, n + 1, n / 10)
+    u = np.zeros(n)
+    for i in range(10):
+        start, end = int(subsets[i]), int(subsets[i + 1])
+        u[start:end] = np.random.uniform(low = i / 10, high = (i + 1) / 10, size = end - start)
+    np.random.shuffle(u)
+    return (sum(map(lambda f : func(a + (b - a) * f) ,u)) * (b - a) / n, time.time() - start1)
 
-
-print("SciPy -> ",integrate.quad(func, 0, math.pi)[0])
-print(monteCarloIntegration(func, 0, math.pi,1000000))
+print("SciPy ->",integrate.quad(func, 0, math.pi)[0])
+a = monteCarloIntegration(func, 0, math.pi)
+print("Monte Carlo One ->", a)
+b = mci(func, 0, math.pi)
+print("Monte Carlo Two ->", b)

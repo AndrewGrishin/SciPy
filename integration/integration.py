5d9dc32583fa0d61c13847b5e4afada16ebc9438
FelixMonteg <FelixMonteg@users.github.com>
2020-10-08 11:05:05 +0300

    Upd

--- integration/integration.py
+++ integration/integration.py
@@ -58,12 +58,12 @@ def mci(func, a = 1, b = 1, n = int(1E7)):
 integrationTask = open("integrationTask.txt","w")
 # use SciPy library (module "integrate", function ".quad") to compare results
 c = integrate.quad(func, 0, math.pi)
-integrationTask.write("SciPy -> " + str(c[0]) + "\n")
+integrationTask.write("SciPy -> " + str(c[0]) + "\n\n")
 a = monteCarloIntegration(func, 0, math.pi)
 # prints to the standart output stream the (values, time, absolute difference comparing to the "a" in %)
-integrationTask.write("Monte Carlo One ->\t" + str(a[0]) + "\n\tExecution time -> " + str(a[1]) + "\n\tDifference (abs) -> " + str(100 * abs(c[0] - a[0]) / c[0]) + "%\n")
+integrationTask.write("Monte Carlo One ->\t" + str(a[0]) + "\n\tExecution time -> " + str(a[1]) + "\n\tDifference (abs) -> " + str(100 * abs(c[0] - a[0]) / c[0]) + " %\n\n")
 b = mci(func, 0, math.pi)
 # prints to the standart output stream the (values, time, absolute difference comparing to the "a" in %)
-integrationTask.write("Monte Carlo One ->\t" + str(b[0]) + "\n\tExecution time -> " + str(b[1]) + "\n\tDifference (abs) -> " + str(100 * abs(c[0] - b[0]) / c[0]) + "%\n")
+integrationTask.write("Monte Carlo One ->\t" + str(b[0]) + "\n\tExecution time -> " + str(b[1]) + "\n\tDifference (abs) -> " + str(100 * abs(c[0] - b[0]) / c[0]) + " %\n")
 # close the file "integrationTask.txt", as all result are already there
 integrationTask.close()

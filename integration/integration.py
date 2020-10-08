60dc3ccd64fdd78a5c6cd048a9234010e67365ef
FelixMonteg <FelixMonteg@users.github.com>
2020-10-08 03:03:04 +0300

    New idea in integration

--- integration/integration.py
+++ integration/integration.py
@@ -1,31 +1,22 @@
 import numpy as np
 from scipy import integrate
+import matplotlib.pyplot as plt
+import math
+import time
 
 def func(x):
-    return 1 / (x**2) + x**3
+    return math.sin(x)
 
-"""def integrate(f,a,b,n):
-    k = (b - a) / n
-    s = 0
-    for i in range(n):
-        s += f(a + k / a + i * k)
-    return s * k
+def monteCarloIntegration(func, a = 0, b = 1, n = 10):
+    xvalues = a + (b - a) * np.random.uniform(low = 0, high = 1, size = n)
+    xvalues += (a + (b - a) * np.random.uniform(low = 0, high = 1, size = n))
+    plt.plot(xvalues)
+    fvalues = np.array(list(map(func,xvalues)))
+    plt.show()
+    print(sum(fvalues * (b - a) / n))
 
-n = 1
-#while (not abs(abs(integrate(function,1,5,n * 2)) - abs(integrate(function,1,5,n))) < 1E-4):
-#    n += 1
-#print(n)"""
+    
 
-def monteCarloIntegration(func, a = 0, b = 1, n = 1000000000):
-    subsets = np.arange(0, n + 1, n / 10)
-    u = np.zeros(n)
-    for i in range(10):
-        s = int(subsets[i])
-        e = int(subsets[i + 1])
-        u[s:e] = np.random.uniform(low = i / 10, high = (i + 1) / 10, size = e - s)
-    np.random.shuffle(u)
-    return (b - a) / n * (func(a + (b - a) * u).sum())
 
-#print(integrate(function,1,10,10000000))
-
-print(abs(abs(integrate.quad(func, 1, 4)[0]) - abs(monteCarloIntegration(func, 1, 4))))
+print("SciPy -> ",integrate.quad(func, 0, math.pi)[0])
+print(monteCarloIntegration(func, 0, math.pi,1000000))

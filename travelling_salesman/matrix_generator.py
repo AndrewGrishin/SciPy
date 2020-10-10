a6ed698a0e95b063ab5d0bbc1d017969be730fc1
FelixMonteg <FelixMonteg@users.github.com>
2020-10-10 17:50:39 +0300

    +

--- travelling_salesman/matrix_generator.py
+++ travelling_salesman/matrix_generator.py
@@ -2,8 +2,8 @@ import numpy as np
 
 file = open("matrix.txt","w")
 
-for line in range(50):
-    na = list(map(str,np.random.randint(low = 2, high = 33, size = 50)))
+for line in range(200):
+    na = list(map(str,np.random.randint(low = 2, high = 33, size = 200)))
     na[line] = "_"
     file.write(" ".join(map(str,na)) + "\n")
 
